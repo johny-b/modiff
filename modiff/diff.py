fe40a80233f4289e8f50f468052fa637884594f8
Jan Betley <jan.betley@gmail.com>
2023-06-11 15:17:37 +0100

    prettier plot

--- modiff/diff.py
+++ modiff/diff.py
@@ -1,6 +1,7 @@
 from functools import cached_property
 from typing import List, Optional, Tuple
 
+import numpy as np
 from jaxtyping import Int, Float
 import plotly.express as px
 from plotly.graph_objects import Figure
@@ -30,21 +31,18 @@ class ModelDiff:
         if len(self.models) != 2:
             raise NotImplementedError("max_attention_diff is implemented only for 2-model scenario")
         
-        #   TODO: ensure models have the same architecture
-        config = self.models[0].cfg
-        n_layers, n_heads = config.n_layers, config.n_heads
-        
+        layers_heads = self._layers_heads()
+
         out_vals = []
         for dataset_caches in self.activation_cache:
             cache_1, cache_2 = dataset_caches
             vals = []
-            for layer in range(n_layers):
-                for head in range(n_heads):
-                    pattern_1 = cache_1["pattern", layer]
-                    pattern_2 = cache_2["pattern", layer]
-                    max_attn_1 = pattern_1[:, head, pos].max(dim=-1).values.mean().item()
-                    max_attn_2 = pattern_2[:, head, pos].max(dim=-1).values.mean().item()
-                    vals.append(max_attn_1 - max_attn_2)
+            for layer, head in layers_heads:
+                pattern_1 = cache_1["pattern", layer]
+                pattern_2 = cache_2["pattern", layer]
+                max_attn_1 = pattern_1[:, head, pos].max(dim=-1).values.mean().item()
+                max_attn_2 = pattern_2[:, head, pos].max(dim=-1).values.mean().item()
+                vals.append(max_attn_1 - max_attn_2)
             out_vals.append(vals)
         return t.tensor(out_vals)
 
@@ -109,7 +107,7 @@ class ModelDiff:
         data = self.correct_token_log_prob_diff.cpu().numpy()
         fig = px.line(data.T)                
         fig.update_layout(
-            title="Difference of log probs of the correct token between models",
+            title="Log probs of the correct token - difference between models",
             legend_title="Problem id",
             xaxis_title="Token position",
             yaxis_title="Correct token log prob diff",
@@ -130,13 +128,16 @@ class ModelDiff:
         return fig
     
     def plot_max_attention_diff(self, pos) -> Figure:
-        data = self.max_attention_diff(pos)
-        fig = px.line(data.T)                
+        data = self.max_attention_diff(pos).tolist()
+        
+        labels = [f"{layer}-{head}" for layer, head in self._layers_heads()]
+        
+        fig = px.line(x=labels, y=data)
         fig.update_layout(
-            title=f"Average log prob value for token",
-            legend_title="Problem id",
-            xaxis_title="Model ix",
-            yaxis_title="Log prob",
+            title=f"Max attention on postion {pos} - difference between models",
+            legend_title="Average max attenton diff",
+            xaxis_title="Head ix",
+            yaxis_title=f"model_0 - model_1 diff of (attn[{pos}].max(-1).mean())",
             title_x=0.5,
         )                    
         return fig
@@ -146,3 +147,12 @@ class ModelDiff:
     def _parse_raw_dataset(self, dataset) -> List[Int[Tensor, "batch seq_len"]]:
         # FIXME
         return dataset
+    
+    def _layers_heads(self) -> List[Tuple[int, int]]:
+        #   TODO: ensure models have the same architecture
+        config = self.models[0].cfg
+        n_layers, n_heads = config.n_layers, config.n_heads
+        out = []
+        for layer in range(n_layers):
+            out += [(layer, head) for head in range(n_heads)]
+        return out
